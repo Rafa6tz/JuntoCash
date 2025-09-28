4882f9567bf7e2a8c3a9359433608d04a0316a87
Rafa6tz <rafa6tz@gmail.com>
2025-09-28 10:12:24 -0300

    do the front end config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  
+  return (
+    <>
+      <Button className="bg-primary">Outline</Button>
+    </>
+  )
+}
+
+export default App
