096d8c88f20f962b61e497ff8c35a825f03570dc
Rafa6tz <rafa6tz@gmail.com>
2025-10-07 20:51:46 -0300

    do the categorie store redux app

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import wallerReducer from "../features/wallet/walletSlice";
 import transactionReducer from "../features/transaction/transactionSlice";
+import categorieReducer from "../features/categories/categorieSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         wallet: wallerReducer,
-        transaction: transactionReducer
+        transaction: transactionReducer,
+        categories: categorieReducer
     }
 })
