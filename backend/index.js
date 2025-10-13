4e8e0f3bb6d9b9265ec5c3db4e67a85b79d2ef95
Rafa6tz <rafa6tz@gmail.com>
2025-10-13 20:54:09 -0300

    trying to connect back with front, vercel with render

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,23 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://junto-cash-thhb-98seydjdu-rafaels-projects-28ebd802.vercel.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.use(require('./routes/userRoute'))
